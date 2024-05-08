ca87d64a33ec2ebb17c330c13a18b3ad0001adb1
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-08 14:22:33 +0200

    Debuuging calculations

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 // Given Parameters
 const velocity = 10000; // velocity (km/h)
-const acceleration = 3; // acceleration (m/s^2)
+const acceleration = -3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
 const distance = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
@@ -16,17 +16,16 @@ const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 const newDistance = distance + (velocity*(time/3600)) //calcultes new distance
 
-const newAcceleration = (acceleration*3600) / 1000 //convert acceleration from m/s^2 to km/h^2
+const newAcceleration = acceleration* (3600 / 1000) //convert acceleration from m/s^2 to km/h^2
 
-//calculates remaining fuel
+//calculates remaining fuelnode
 const remainingFuel = fuelBurnRate*time 
 const newRemainingFuel = fuel - remainingFuel 
 
-const newVelocity = calcNewVel(acceleration, velocity, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 function calcNewVel(velocity, acceleration, time) { 
-  if (typeof velocity !== 'number' || typeof acceleration !== 'number' || typeof time !== 'number') {
+  if (velocity !== number || acceleration !== number || time !== number) {
     throw new Error ("Invalid input: Parameters must be numbers.");
   }
   if (time < 0) {
@@ -34,6 +33,9 @@ function calcNewVel(velocity, acceleration, time) {
   }
   return velocity + (newAcceleration * time)
 }
+
+const newVelocity = calcNewVel(velocity, acceleration, time) //calculates new velocity based on acceleration
+
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
