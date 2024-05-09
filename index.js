27ffd14c79ed0f89c91ae0dd6c5a043e886a8b17
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-09 16:02:28 +0200

    Implementing props to the code

--- index.js
+++ index.js
@@ -6,35 +6,80 @@
  */
 
 // Given Parameters
-const velocity = 10000; // velocity (km/h)
-const acceleration = -3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const distance = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
+const props = {
+  // velocity (km/h)
+ velocity : {
+  value: 10000,
+  measurement: "km/h"
+ }, 
 
+ // acceleration (m/s^2)
+ acceleration : {
+  value: 3,
+  measurement: "m/s^2"
+},
 
-const newDistance = distance + (velocity*(time/3600)) //calcultes new distance
+// seconds (1 hour)
+ time : {
+  value: 3600,
+  measurement: "s"
+ },
 
-const newAcceleration = acceleration* (3600 / 1000) //convert acceleration from m/s^2 to km/h^2
+// distance (km)
+ distance : {
+  value: 0, 
+  measurement: "km"
+ },
+
+ // remaining fuel (kg)
+ fuel : {
+  value:5000, 
+  measurement: "kg"
+ },
+
+ // fuel burn rate (kg/s)
+ fuelBurnRate : {
+  value: 0.5 ,
+  measurement: "kg/s"
+},
+}
+
+const newDistance = props.distance.value + (props.velocity.value*(props.time.value/3600)) //calcultes new distance
 
 //calculates remaining fuelnode
-const remainingFuel = fuelBurnRate*time 
-const newRemainingFuel = fuel - remainingFuel 
+const remainingFuel = props.fuelBurnRate.value*props.time.value
+const newRemainingFuel = props.fuel.value - remainingFuel 
 
 
 // Pick up an error with how the function below is called and make it robust to such errors
-function calcNewVel(velocity, acceleration, time) { 
-  if (velocity !== number || acceleration !== number || time !== number) {
+function calculateNewVelocity(props) { 
+
+  const {velocity: {value: velocity}} = props;
+  const {acceleration: {value: acceleration}} = props;
+  const {time: {value: time}} = props;
+
+  const {velocity: {measurement: velocityMeasurement}} = props;
+  const {acceleration: {measurement: accelerationMeasurement}} = props;
+  const {time: {measurement: timeMeasurement}} = props;
+
+  const newAcceleration = acceleration* (3600 / 1000) //convert acceleration from m/s^2 to km/h^2
+
+  if (typeof velocity !== 'number' || typeof acceleration !== 'number' ||typeof time !== 'number') {
     throw new Error ("Invalid input: Parameters must be numbers.");
   }
+
+  if (velocityMeasurement !== "km/h" || accelerationMeasurement !== "m/s^2" || timeMeasurement !== "s") {
+    throw new Error ("Invalid input: Correct measurement required.")
+  }
+
   if (time < 0) {
     throw new Error ("Invalid input: Time cannot be negative.");
   }
+
   return velocity + (newAcceleration * time)
 }
 
-const newVelocity = calcNewVel(velocity, acceleration, time) //calculates new velocity based on acceleration
+const newVelocity = calculateNewVelocity(props) //calculates new velocity based on acceleration
 
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
